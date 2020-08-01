6b6a69c0f89363c932b797dfb82270f6e06bc5a0
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2020-08-01 19:27:15 -0400

    move

--- olgymiomproj/olgymiomproj/settings.py
+++ olgymiomproj/olgymiomproj/settings.py
@@ -121,12 +121,11 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-if APP_PHASE == "dev":
-    STATIC_URL = "/static/"
-if APP_PHASE == "production":
-    AWS_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# STATIC_URL = "/static/"
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 if APP_PHASE == "production":
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SESSION_COOKIE_SECURE = True
